173cb39df3a780cbd2ebe8bfbf77c20346cb500c
Bando <eliudmugo360@gmail.com>
2026-03-22 22:34:26 +0300

    feat: add X gate circuit using Qiskit 2.0 primitives

--- 03_x_gate/x_gate.py
+++ 03_x_gate/x_gate.py
@@ -1,13 +1,12 @@
 from qiskit import QuantumCircuit
+from qiskit.primitives import StatevectorSampler
 
-# Create a circuit with 1 qubit and 1 classical bit
 qc = QuantumCircuit(1, 1)
-
-# Apply X gate (quantum NOT - flips |0> to |1>)
 qc.x(0)
-
-# Measure the qubit
 qc.measure(0, 0)
 
-# Draw the circuit
-print(qc.draw())
+sampler = StatevectorSampler()
+job = sampler.run([qc])
+result = job.result()
+
+print(result[0].data.c.get_counts())
